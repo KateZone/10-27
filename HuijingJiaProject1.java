95cd69ea9761bf19e2dd68ef0e553561c39f3e46
KateZone <134741223+KateZone@users.noreply.github.com>
2024-10-27 23:09:42 -0700

    Add files via upload

---
+++ HuijingJiaProject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package HuijingJiaProject1;
+
+/**
+ *
+ * @author katejia
+ */
+public class HuijingJiaProject1 {
+
+    public static void main(String[] args) {
+        int[] squares = {0,1,4,9,16};
+        System.out.println("Hello World!{int}");
+    }
+}
+
+
